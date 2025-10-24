64bba5043f2cb2e411c52aa1f560b79d99251d8e
Abilash N <abilashnarayanan2001@gmail.com>
2025-10-24 23:54:05 +0530

    Updated images

--- src/components/Sections/HeroSection.jsx
+++ src/components/Sections/HeroSection.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import { motion, useScroll, useTransform } from "framer-motion";
+import { ArrowDown, Mail } from "lucide-react";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { useTheme } from "../../context/ThemeContext";
+import luffy from "../../assets/images/luffy.jng";
 const HeroSection = () => {
-  return <div className="text-white">HeroSection</div>;
+  return (
+    <div className="p-8">
+      <img
+        src={luffy}
+        alt="Luffy"
+        className="w-40 h-40 object-cover rounded-full"
+      />
+    </div>
+  );
 };
 
 export default HeroSection;
---
+++ src/context/ThemeContext 2.jsx
@@ -0,0 +1,1 @@
+import { createContext, t } from "react";
